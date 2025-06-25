d8bdd84bd4b3ef76c57dbd7261e9e954505be367
Yeryeong Kang <dpfud8786@gmail.com>
2025-06-25 17:49:04 +0900

    Character 이미지 및 컴포넌트 수정

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
+import Logo from '../assets/logo-character.svg?react';
 
-function Character({ size = 'w-24 h-24' }) {
-  return (
-    <div className={`${size} rounded-full overflow-hidden`}>
-      <img
-        src="/character.png"
-        alt="character"
-        className="w-full h-full object-cover"
-      />
-    </div>
-  );
+function Character() {
+  return <Logo className="w-20 h-20" />;
 }
 
 export default Character;
