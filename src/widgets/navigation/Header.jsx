b9354ba4311b782efb07bd556dfda62b48c3c3c9
Nam Yooseong <nys8765@naver.com>
2025-09-16 02:21:50 +0900

    style: 헤더 스타일, 디자인 개편

--- src/widgets/navigation/Header.jsx
+++ src/widgets/navigation/Header.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo-character2.svg?react';
 import MyTrackHeader from '../../assets/mytrack-header.svg?react';
-import TrackIcon from '../../assets/track.svg?react';
-import SearchIcon from '../../assets/search.svg?react';
-import MypageIcon from '../../assets/mypage.svg?react';
+// import TrackIcon from '../../assets/track.svg?react';
+// import SearchIcon from '../../assets/search.svg?react';
+// import MypageIcon from '../../assets/mypage.svg?react';
 
 function Header() {
   return (
-    <header className="w-full h-[70px] bg-blue-600 px-6 flex items-center">
-      <div className="w-full flex justify-between items-center">
+    <header className="fixed left-0 top-0 z-20 w-full h-[60px] bg-blue-primary">
+      <div className="mx-auto flex h-full max-w-[1280px] justify-between items-center px-4 md:px-8">
         {/* 왼쪽 로고 */}
         <Link to="/home">
           <div className="flex items-center gap-2">
@@ -20,13 +20,18 @@ function Header() {
         {/* 오른쪽 아이콘들 */}
         <div className="flex items-center gap-8">
           <Link to="/track">
-            <TrackIcon className="w-6 h-6" />
+            {/* <TrackIcon className="w-6 h-6" /> */}
+            <span className="inline-block text-white font-semibold hover:text-blue-light-3 hover:scale-103 transform transition-all">
+              서비스 소개
+            </span>
           </Link>
-          <Link to="/search">
+          {/* <Link to="/search">
             <SearchIcon className="w-6 h-6" />
-          </Link>
-          <Link to="/mypage">
-            <MypageIcon className="w-6 h-6" />
+          </Link> */}
+          <Link to="/login">
+            <span className="inline-block text-white font-semibold hover:text-blue-light-3 hover:scale-103 transform transition-all">
+              로그인
+            </span>
           </Link>
         </div>
       </div>
