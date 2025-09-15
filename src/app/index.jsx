9bd28c143538383f23fa9cef626b6913332e1793
Nam Yooseong <nys8765@naver.com>
2025-09-16 02:48:46 +0900

    refactor: 레이아웃 구조 개선

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,16 +1,13 @@
 // 전역 컨텍스트 및 상태 관리 설정 (라우팅, 인증 등)
-
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
   Navigate,
 } from 'react-router-dom';
-
 import Header from '../widgets/navigation/Header';
 import Footer from '../widgets/navigation/Footer';
-
 import Home from '../pages/home';
 import TrackInfo from '../pages/info';
 import NotFound from '../pages/NotFound';
@@ -18,13 +15,13 @@ import Search from '../pages/Search';
 import Splash from '../pages/splash';
 import Login from '../pages/login';
 import MyPage from '../pages/my';
-import TrackDataUploadModal from '../features/track-management/components/TrackDataUploadModal';
+// import TrackDataUploadModal from '../features/track-management/components/TrackDataUploadModal';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <AppContent />
-    </BrowserRouter>
+    </Router>
   );
 }
 
@@ -34,19 +31,23 @@ function AppContent() {
   const shouldHideHeaderFooter = hiddenRoutes.includes(location.pathname);
 
   return (
-    <div className="min-h-screen bg-blue-primary">
+    <div className="flex flex-col min-h-screen">
       {!shouldHideHeaderFooter && <Header />}
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/splash" element={<Splash />} />
-        <Route path="/track" element={<TrackInfo />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/upload" element={<TrackDataUploadModal />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <main
+        className={`w-full flex-grow ${!shouldHideHeaderFooter ? 'pt-[60px]' : ''}`}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/splash" element={<Splash />} />
+          <Route path="/track" element={<TrackInfo />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/mypage" element={<MyPage />} />
+          {/* <Route path="/upload" element={<UploadSection />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
       {!shouldHideHeaderFooter && <Footer />}
     </div>
   );
